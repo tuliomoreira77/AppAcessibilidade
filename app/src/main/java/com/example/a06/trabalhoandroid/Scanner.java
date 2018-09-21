1c00430d183f0cff73b86798e971e53ff8cf3866
Tulio <tuliomoreira77@gmail.com>
2018-09-21 12:04:48 -0300

    Version 0.2

--- app/src/main/java/com/example/a06/trabalhoandroid/Scanner.java
+++ app/src/main/java/com/example/a06/trabalhoandroid/Scanner.java
@@ -3,14 +3,12 @@ package com.example.a06.trabalhoandroid;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Scanner extends Fragment {
 
 
@@ -23,7 +21,11 @@ public class Scanner extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Fragment scanerView = new ScanerView();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, scanerView).commit();
         return inflater.inflate(R.layout.fragment_scanner, container, false);
+
     }
 
 }
